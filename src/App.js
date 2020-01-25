4f37c85cdda799fc8f9e92fe5f70478a126b4fca
Ben Hernandez <benjammin234@gmail.com>
2020-01-24 17:31:17 -0800

    Adds search filter options

--- src/App.js
+++ src/App.js
@@ -2,6 +2,37 @@ import React from 'react';
 import Search from './components/Search/Search';
 
 export default class App extends React.Component {
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {
+  //     books: [],
+  //     showBooks: false
+  //   };
+  // }
+
+  componentDidMount() {
+    const url = 'https://www.googleapis.com/books/v1/volumes?q=search+terms';
+    const options = {
+      method: 'GET'
+    }
+  }
+
+  // fetch(url, options)
+  //   .then(res => {
+  //     if(!res.ok) {
+  //       throw new Error('No such book, we heard you can\'t read anyway.')
+  //     }
+  //     return res;
+  //   })
+  //   .then(res => res.json())
+  //   .catch(err => {
+  //     this.ListeningStateChangedEvent({
+  //       error: err.message
+  //     });
+  //   });
+
+
+
   render() {
     return (
       <div className='App'>
--- src/components/SearchFilters/SearchFilters.js
+++ src/components/SearchFilters/SearchFilters.js
@@ -6,15 +6,19 @@ export default class SearchFilters extends React.Component {
             <div className='SearchFilters'>
                 <label htmlFor='print-type'>Print Type:</label>
                 <select name='print-type'>
-                    <option value='All'>All</option>
-                    <option value='Book'>Book</option>
-                    <option value='Not-a-book'>Not a book</option>
+                    <option value='all'>All</option>
+                    <option value='books'>Books</option>
+                    <option value='magazines'>Magazines</option>
                 </select>
 
                 <label htmlFor='book-type'>Book Type:</label>
                 <select name='book-type'>
                     <option value='No-filter'>No filter</option>
-                    <option value='eBook'>eBook</option>
+                    <option value='Partial'>Partial</option>
+                    <option value='Full'>Full</option>
+                    <option value='free-ebooks'>Free eBooks</option>
+                    <option value='paid-ebooks'>Paid eBooks</option>
+                    <option value='ebooks'>eBooks</option>
                 </select>
             </div>
         )
