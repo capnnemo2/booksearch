3e164605fc839cfd3109c37503d324845c45dcba
Ben Hernandez <benjammin234@gmail.com>
2020-01-24 17:17:19 -0800

    Add static search components

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import SearchFilters from '../SearchFilters/SearchFilters';
 
 export default class Search extends React.Component {
     render() {
         return (
             <div className='Search'>
+                <form className='Search__form'>
+                    <label htmlFor='search'>Search:</label>
+                    <input
+                        type='text'
+                        name='search'
+                        id='search'
+                        placeholder='potato'
+                        value='' />
+                    <button type='submit'>Search</button>
+                    <SearchFilters />
+                </form>
 
             </div>
         )
--- src/components/SearchFilters/SearchFilters.js
+++ src/components/SearchFilters/SearchFilters.js
@@ -4,7 +4,18 @@ export default class SearchFilters extends React.Component {
     render() {
         return (
             <div className='SearchFilters'>
+                <label htmlFor='print-type'>Print Type:</label>
+                <select name='print-type'>
+                    <option value='All'>All</option>
+                    <option value='Book'>Book</option>
+                    <option value='Not-a-book'>Not a book</option>
+                </select>
 
+                <label htmlFor='book-type'>Book Type:</label>
+                <select name='book-type'>
+                    <option value='No-filter'>No filter</option>
+                    <option value='eBook'>eBook</option>
+                </select>
             </div>
         )
     }
