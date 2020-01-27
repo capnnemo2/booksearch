59bb86ce2cdbf7ed5044eda89fe9d11ac1aac00a
Ben Hernandez <benjammin234@gmail.com>
2020-01-26 21:34:58 -0800

    Adds to-do list comment

--- src/components/Booksearch/Booksearch.js
+++ src/components/Booksearch/Booksearch.js
@@ -68,7 +68,12 @@ export default class Booksearch extends React.Component {
             })
     }
 
-    
+    // need to find a way to send results: data to ResultList
+    // ResultList can then filter the array and pick out the info we want to display for the initial list (IE: title, author, ebook, ??)
+    // Upon clicking the book (the whole li in a ul? inline-block? or just clicking the title? or clicking the book's image?) a conditional statement will display further details about the book: price?, partial vs full?, ??
+    // Clicking the book again will hide the extra info
+
+
 
 
     render() {
